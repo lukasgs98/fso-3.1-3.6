ba0b4b73ed88c4bf293a26eef168dd0ddc050fe4
Lukas Geissler <lukasgs_@outlook.de>
2025-06-27 10:45:16 +0200

    Add CORS

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 // CONFIGURE SERVER
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 // CONFIGURE MIDDLEWARE
 morgan.token("req-body", (req, res) => JSON.stringify(req.body))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body"))
 
