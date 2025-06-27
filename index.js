ff359e8ccf4f30c365af6626d13c19049db864b1
Lukas Geissler <lukasgs_@outlook.de>
2025-06-27 11:04:22 +0200

    Add middleware for static files

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001
 
 // CONFIGURE MIDDLEWARE
 morgan.token("req-body", (req, res) => JSON.stringify(req.body))
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body"))
